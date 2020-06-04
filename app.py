6ee7e971402f25831501e596efd8c9d6242be255
gowthamaraj <gowthamaraj360@gmail.com>
2020-06-05 01:58:24 +0530

    bug fix: deployment app not giving data

--- app.py
+++ app.py
@@ -76,5 +76,4 @@ def excel():
         return "Error: File cannot be generated at the moment"  
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(threaded=True, port=5000)
